297e3b8630fc62a32524ecd19514a194b57711cf
Nick Cavalcanti <120645786+Nichbreeze@users.noreply.github.com>
2023-02-07 21:08:36 -0300

    Update ProdutoRepository.java

--- Farmacia/src/main/java/com/generation/Farmacia/repository/ProdutoRepository.java
+++ Farmacia/src/main/java/com/generation/Farmacia/repository/ProdutoRepository.java
@@ -1,14 +1,12 @@
-package com.generation.Farmacia.repository;
-
+package org.generation.farmacia.repository;
 
 import java.util.List;
 
+import org.generation.farmacia.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.generation.Farmacia.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	
-	public List<Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
-
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
 }
