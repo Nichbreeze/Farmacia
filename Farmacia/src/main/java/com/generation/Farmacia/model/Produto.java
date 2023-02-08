242b96fb73df4d6c8a65de5f695c91f6cda967d2
Nick <nicholas.cavalcante05@gmail.com>
2023-02-07 21:00:18 -0300

    Exercicio Farmacia

---
+++ Farmacia/src/main/java/com/generation/Farmacia/model/Produto.java
@@ -0,0 +1,56 @@
+package com.generation.Farmacia.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nomeProduto;
+	@NotNull
+	private double preco;
+	@ManyToOne
+	@JsonIgnoreProperties("Produto")
+	private Categoria categoria;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
+
---
+++ Farmacia/src/main/java/com/generation/Farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.generation.Farmacia.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.Farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+
+}
